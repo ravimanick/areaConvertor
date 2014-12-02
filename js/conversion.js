f1079515991d43b6ab055aad512d8aca1965a41a
ravimanick <ravimanik@gmail.com>
2014-12-02 14:15:25 +0530

    new add

---
+++ js/conversion.js
@@ -0,0 +1,19 @@
+//JS for area calculations
+// to find conversion hec to Acre
+
+var areaCalculator = {
+
+
+	areaCalculator.convertAcre {
+			acre : 0,
+
+			findAcre:function (){
+			hec = parseInt(document.getElementById("hec").value;
+			acre = hec * 2.47105;
+			document.getElementById("acr").value = this.acre;
+			alert(typeof(hec));
+			}
+
+	};
+
+
